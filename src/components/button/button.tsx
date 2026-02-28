ec13f9606f6441fff7074d58563817c130790e7b
draxart7 <draxart7@gmail.com>
2026-02-27 17:18:47 -0700

    added disable and default variant to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,25 @@
 import "./button.css";
-export const Button = ({ variant = "default" }) => {
-  return <button className={`Button ${variant}`}>Hello</button>;
+interface ButtonProps {
+  variant?: string;
+  title?: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+export const Button = ({
+  variant = "default",
+  title = "",
+  disabled = false,
+  onClick = () => {
+    console.log(title);
+  },
+}: ButtonProps) => {
+  return (
+    <button
+      className={`Button ${variant} ${disabled}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {title}
+    </button>
+  );
 };
--- src/components/preview/preview.tsx
+++ src/components/preview/preview.tsx
@@ -1,10 +1,22 @@
+import "./preview.css";
 import { Button } from "../button/button";
 export const Preview = () => {
   return (
     <div className="Preview">
-      Preview
-      <div>
-        <Button />
+      <div className="Header"> Preview Page </div>
+      <div className="LeftRail"> Left Rail</div>
+      <div className="Content">
+        <div className="SectionButtons">
+          <Button
+            variant="action"
+            title="Action"
+            onClick={() => {
+              console.log("Action");
+            }}
+          />
+          <Button title="Default" />
+          <Button title="Disabled" disabled />
+        </div>
       </div>
     </div>
   );
