3f5c2755960f4762c15f4c3393f51437116c731d
draxart7 <draxart7@gmail.com>
2026-03-01 11:46:33 -0700

    added mobile drop down

--- src/components/header-link-group-mobile/header-link-group-mobile.tsx
+++ src/components/header-link-group-mobile/header-link-group-mobile.tsx
@@ -1,18 +1,66 @@
 import "./header-link-group-mobile.css";
 import { BurgerSvg } from "../../assets/svgs/burger-svg";
 import { Button } from "../button/button";
+import React, { useState, useEffect, useRef } from "react";
+import { HeaderLink } from "../header-link/header-link";
 
 export const HeaderLinkGroupMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLInputElement>(null);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  // Close the dropdown when clicking outside of it
+  useEffect(() => {
+    const handleClickOutside = (event: Event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className={`HeaderLinkGroupMobile`}>
+    <div className={`HeaderLinkGroupMobile`} ref={dropdownRef}>
       <Button
         isQuiet
-        onClick={() => {
-          console.log("open draw");
-        }}
+        onClick={toggleDropdown}
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+        aria-controls="dropdown-menu"
       >
         <BurgerSvg />
       </Button>
+      {isOpen && (
+        <ul
+          id="dropdown-menu"
+          role="menu"
+          aria-labelledby="dropdown-button"
+          className="dropdown-menu"
+        >
+          <li role="menuitem">
+            <HeaderLink title="Intro" />
+          </li>
+          <li role="menuitem">
+            <HeaderLink title="About" />
+          </li>
+          <li role="menuitem">
+            <HeaderLink title="Projects" />
+          </li>
+          <li role="menuitem">
+            <HeaderLink title="Skills" />
+          </li>
+          <li role="menuitem">
+            <HeaderLink title="Contact" />
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
