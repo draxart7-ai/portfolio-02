dbf41810f9349d538a082849140a0126a22bf5ce
draxart7 <draxart7@gmail.com>
2026-03-04 20:57:11 -0700

    tooking diplay out of cards

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -15,38 +15,42 @@ export const About = () => {
 
   return (
     <div id="About" className={`About ${visible} section`} ref={sectionRef}>
-      <div className="title" style={{ fontSize: "28px", fontWeight: "bold" }}>
+      <div className="title">
         <Card>
-          <p>About Me</p>
+          <div className="header">
+            <p>About Me</p>
+          </div>
         </Card>
       </div>
       <div className="desc">
         <Card>
-          <p style={{ padding: "0px 20px", fontSize: "32px" }}>
-            Software Developer | Technical Artist{" "}
-          </p>
-          <p style={{ padding: "0px 20px" }}>
-            Placeholder! I approach my work and personal growth through the lens
-            of internal composure and steady, repetitive practice. I view myself
-            as an explorer of systems, driven by a curiosity to understand how
-            diverse pieces connect and function together.
-          </p>
-          <p style={{ padding: "10px 20px" }}>
-            Placeholder! chasing milestones, I focus on the daily discipline of
-            the process, prioritizing efficiency and objective logic in every
-            challenge. My commitment lies in maintaining a grounded perspective
-            and a quiet, consistent drive to refine my craft.
-          </p>
-          <p style={{ padding: "0px 20px" }}>
-            Placeholder! in focused work, where efficiency is the only real
-            measure of progress. I seek the clarity of a well ordered mind and a
-            task executed with precision.
-          </p>
+          <div className="container">
+            <p className="title">Mission </p>
+            <p>
+              Empower people through creating intuitive frictonless software
+            </p>
+            <p className="title">Software Developer</p>
+            <p>
+              I have been a software developer for the last 10years. Main
+              achievements:
+              <br />- Adobe: Inventor of the Ai Reviewer
+              <br />- Developing Virtual Reality
+              <br />- Developing Virtual Reality
+            </p>
+            <p className="title">Technical Artist </p>
+            <p>
+              Placeholder! in focused work, where efficiency is the only real
+              measure of progress. I seek the clarity of a well ordered mind and
+              a task executed with precision.
+            </p>
+          </div>
         </Card>
       </div>
       <div className="picture">
         <Card>
-          <img src="/images/misc/alan-01.jpg" alt="profile" height="300px" />
+          <div className="container">
+            <img src="/images/misc/alan-01.jpg" alt="profile" />
+          </div>
         </Card>
       </div>
       <div
