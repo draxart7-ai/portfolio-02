75c10afe3a946e56000e27028e8adac838ff3b57
Alan Curtis <draxart7@gmail.com>
2026-04-25 21:48:39 -0600

    adding ghostbusters

--- src/components/projects/projects-data.ts
+++ src/components/projects/projects-data.ts
@@ -23,7 +23,6 @@ export const projectsData = [
       "Photoshop",
       "Nodejs",
       "UX",
-      "Express",
       "Vite",
     ],
     achievements: [
@@ -43,6 +42,25 @@ export const projectsData = [
     "/images/projects/paint-your-picture/paint-your-picture-02.jpg",
     "/images/projects/paint-your-picture/paint-your-picture-03.jpg",
     "/images/projects/paint-your-picture/paint-your-picture-04.jpg",
+  ],
+    links: [],
+    description:
+      "This was a site challenge to match the css style requirements from an image.",
+    tags: ["React", "CSS", "Nodejs"],
+    achievements: [
+      "Made functional site from static image within a given time frame.",
+      "Leaned new css tricks. The corner fold.",
+      "Side bar fades on page scroll position.",
+      "Used gsap to make cool staggered animations and transitions.",
+    ],
+  },
+    {
+    id: 2,
+    priority: 11,
+    title: "Ghostbusters",
+    media:  [
+    "/images/projects/ghostbusters/ghostbusters-01.jpg",
+    "/images/projects/ghostbusters/ghostbusters-02.jpg",
   ],
     links: [],
     description:
